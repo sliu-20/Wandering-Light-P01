c9ed7aa782c53fdb653206a196cda1393f0b53dc
Jonathan Wu <jwu20@stuy.edu>
2021-12-15 00:03:17 -0500

    added a few fxn routes and db

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ import sqlite3
 import json
 import ssl
 import urllib.request
+from random import randint
 from flask import Flask, render_template, request, session, redirect
 
 MAIN_DB = "vacation.db"
@@ -19,8 +20,19 @@ c = db.cursor()
 
 # Vacation Ideas table creation
 c.execute("""
-CREATE TABLE IF NOT EXISTS VACATIONDATA (
+CREATE TABLE IF NOT EXISTS SAVED (
     USERID     INTEGER,
+    COUNTRY    TEXT    NOT NULL,
+    FORECAST   TEXT    NOT NULL,
+    HOLIDAYS   TEXT    NOT NULL,
+    ACTIVITY   TEXT    NOT NULL,
+    BOOKS      TEXT    NOT NULL,
+    MEDIA      TEXT    NOT NULL
+);""")
+
+# Storing data table creation
+c.execute("""
+CREATE TABLE IF NOT EXISTS VACATIONDATA (
     COUNTRY    TEXT    NOT NULL,
     FORECAST   TEXT    NOT NULL,
     HOLIDAYS   TEXT    NOT NULL,
@@ -41,8 +53,10 @@ CREATE TABLE IF NOT EXISTS USERS (
 db.commit()
 db.close()
 
-app = Flask(__name__) #create instance of class Flask
-app.secret_key = os.urandom(32)
+app = Flask(__name__) # create instance of class Flask
+app.secret_key = os.urandom(32) # generates a string containing random characters
+
+ssl._create_default_https_context = ssl._create_unverified_context # bypasses the 'certificate verify failed: certificate has expired' error on machines.
 
 def api_store(): # accesses the apis and stores the data into the VACATIONDATA table
     db = sqlite3.connect(MAIN_DB)
@@ -53,13 +67,22 @@ def api_store(): # accesses the apis and stores the data into the VACATIONDATA t
     d_data = read_data.decode('utf-8')
     p_data = json.loads(d_data)
 
-    print(p_data[0])
-    c.execute("""INSERT INTO VACATIONDATA (COUNTRY) VALUES (?);""", p_data)
+    # c.execute("""INSERT INTO VACATIONDATA (COUNTRY) VALUES();""", p_data[0])
+    # db.commit()
+    # db.close()
 
-@app.route("/") # assign fxn to route
+@app.route("/") # assign fxn to route for home page
 def home_page():
     api_store()
     return render_template("index.html")
+
+@app.route("/login") # assign fxn to route for login page
+def login_page():
+    return render_template("login.html")
+
+@app.route("/register") # assign fxn to route for register page
+def register_page():
+    return render_template("register.html")
 
 if __name__ == "__main__": # true if this file is NOT imported
     app.debug = True       # enable auto-reload upon code change
